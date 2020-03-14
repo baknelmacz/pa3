06654b4498b51b1698c508180cfa08e4b339edc4
baknelmacz <baknelmacz@gmail.com>
2020-03-14 11:37:06 -0700

    initial commit

---
+++ stats.cpp
@@ -0,0 +1,39 @@
+
+#include "stats.h"
+
+stats::stats(PNG & im){
+
+// YOUR CODE HERE!!
+
+}
+
+long stats::getSum(char channel, pair<int,int> ul, pair<int,int> lr){
+
+// YOUR CODE HERE!!
+
+}
+
+long stats::getSumSq(char channel, pair<int,int> ul, pair<int,int> lr){
+
+// YOUR CODE HERE!!
+
+}
+
+long stats::rectArea(pair<int,int> ul, pair<int,int> lr){
+
+// YOUR CODE HERE!!
+
+}
+
+// given a rectangle, compute its sum of squared deviations from mean, over all color channels.
+long stats::getScore(pair<int,int> ul, pair<int,int> lr){
+
+// YOUR CODE HERE!!
+
+}
+		
+RGBAPixel stats::getAvg(pair<int,int> ul, pair<int,int> lr){
+
+// YOUR CODE HERE!!
+
+}
